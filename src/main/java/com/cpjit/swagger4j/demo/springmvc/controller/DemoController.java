11c813acab1a1eef418e5395d2453356797473a6
yonghuan <2232911026@qq.com>
2018-04-20 09:01:37 +0800

    APISchemas注解使用示例

--- src/main/java/com/cpjit/swagger4j/demo/springmvc/controller/DemoController.java
+++ src/main/java/com/cpjit/swagger4j/demo/springmvc/controller/DemoController.java
@@ -1,7 +1,7 @@
 /*
  * Copyright 2011-2017 CPJIT Group.
- * 
- * 
+ *
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,64 +12,49 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License. 
+ * limitations under the License.
  */
 package com.cpjit.swagger4j.demo.springmvc.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-
-import com.alibaba.fastjson.JSONWriter;
 import com.cpjit.swagger4j.annotation.API;
 import com.cpjit.swagger4j.annotation.APIs;
 import com.cpjit.swagger4j.annotation.DataType;
 import com.cpjit.swagger4j.annotation.Param;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author yonghuan
  */
-@Controller
+@RestController
 @RequestMapping("/demo")
 @APIs("/demo")
 public class DemoController {
-	@API(value="login", summary="示例1", parameters={
-			@Param(name="username", description="用户名", dataType=DataType.STRING, defaultValue = "admin"),
-			@Param(name="password", description="密码", dataType=DataType.PASSWORD),
-			@Param(name = "sex", description = "性别", dataType = DataType.ARRAY, items = "sex"),
-			@Param(name = "age", description = "年龄", dataType = DataType.INTEGER, defaultValue = "18"),
-			@Param(name="image" , description="图片", dataType=DataType.FILE)
-	})
-	@RequestMapping(value="login", method=RequestMethod.POST)
-	public void login(@RequestParam Map<String, Object> param, MultipartFile image, HttpServletResponse response) throws Exception {
-		response.setContentType("application/json;charset=utf-8");
-		JSONWriter writer = new JSONWriter(response.getWriter());
-		writer.writeObject(param);
-		writer.flush();
-		writer.close();
-	}
-	
-	@API(value="logout", summary="示例2", parameters={
-			@Param(name="username", description="用户名", dataType=DataType.STRING),
-			@Param(name="password", description="密码", dataType=DataType.PASSWORD),
-			@Param(name="image" , description="图片", dataType=DataType.FILE)
-	})
-	@RequestMapping(value="logout", method=RequestMethod.POST)
-	public void logout(HttpServletResponse response, String username, String password, MultipartFile image) throws Exception {
-		response.setContentType("application/json;charset=utf-8");
-		JSONWriter writer = new JSONWriter(response.getWriter());
-		Map<String, String> user = new HashMap<String, String>();
-		user.put("username", username);
-		user.put("password", password);
-		writer.writeObject(user);
-		writer.flush();
-		writer.close();
-	}
+    @API(value = "login", summary = "示例1", parameters = {
+            @Param(schema = "user", in = "body"),
+    })
+    @RequestMapping(value = "login", method = RequestMethod.POST)
+    public Object login(@RequestBody Map<String, Object> param) throws Exception {
+        return param;
+    }
+
+    @API(value = "logout", summary = "示例2", parameters = {
+            @Param(name = "username", description = "用户名", dataType = DataType.STRING),
+            @Param(name = "password", description = "密码", dataType = DataType.PASSWORD),
+            @Param(name = "image", description = "图片", dataType = DataType.FILE)
+    })
+    @RequestMapping(value = "logout", method = RequestMethod.POST)
+    public Object logout(String username, String password, MultipartFile image) throws Exception {
+        Map<String, String> user = new HashMap<>();
+        user.put("username", username);
+        user.put("password", password);
+        return user;
+    }
 }
--- src/main/java/com/cpjit/swagger4j/demo/springmvc/controller/package-info.java
+++ src/main/java/com/cpjit/swagger4j/demo/springmvc/controller/package-info.java
@@ -1,7 +1,7 @@
 /*
  * Copyright 2011-2017 CPJIT Group.
- * 
- * 
+ *
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,7 +12,7 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License. 
+ * limitations under the License.
  */
 /**
  * @author yonghuan
@@ -21,10 +21,14 @@
 @Items(items = {
         @Item(value = "sex", type = "string", optionalValue = {"男", "女", "未知"}, defaultValue = "未知")
 })
+
+@APISchemas(schemas = {
+        @APISchema(value = "user", xml = "user",
+                properties = {@APISchemaPropertie(value = "username", type = "string", format = "", description = "用户名"),
+                        @APISchemaPropertie(value = "password", type = "string", format = "password", description = "登录密码")}
+        )
+})
 package com.cpjit.swagger4j.demo.springmvc.controller;
 
-import com.cpjit.swagger4j.annotation.APITag;
-import com.cpjit.swagger4j.annotation.APITags;
-import com.cpjit.swagger4j.annotation.Item;
-import com.cpjit.swagger4j.annotation.Items;
+import com.cpjit.swagger4j.annotation.*;
 
